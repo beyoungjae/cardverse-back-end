b38313215653e83f7f73b7b8a232b87b95504b7d
jo4086 <ghzktm5892@gmail.com>
2025-03-06 17:38:25 +0900

    Date: 2025-03-06, 17h 38m / File: null / Msg: 푸쉬새로

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,34 +1,26 @@
 const winston = require('winston')
-const DailyRotateFile = require('winston-daily-rotate-file')
-const path = require('path')
 
-// 로그 폴더 경로
-const logDir = 'log'
-
-// 로그 포맷 설정
-const logFormat = winston.format.combine(
-   winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-   winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}] ${message}`
-   }),
-)
-
-// 로그 파일 설정
-const transport = new DailyRotateFile({
-   filename: path.join(logDir, '%DATE%.log'),
-   datePattern: 'YYYY-MM-DD',
-   zippedArchive: false,
-   maxSize: '20m',
-   maxFiles: '7d',
-   dirname: logDir,
-})
-
-// Winston 로거 생성
 const logger = winston.createLogger({
-   level: 'info',
-   format: logFormat,
-   transports: [transport, new winston.transports.Console()],
+   level: 'debug',
+   format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.printf((info) => {
+         let message = info.message
+
+         if (typeof message === 'object') {
+            message = JSON.stringify(message, null, 4)
+         }
+
+         const args = info[Symbol.for('splat')] || []
+         if (args.length > 0) {
+            const argString = args.map((arg) => (typeof arg === 'object' ? JSON.stringify(arg, null, 2) : String(arg))).join(' ')
+            message = `${message} ${argString}`
+         }
+
+         return `${info.level}: ${message}`
+      }),
+   ),
+   transports: [new winston.transports.Console()],
 })
 
-// 로거 내보내기
 module.exports = logger
